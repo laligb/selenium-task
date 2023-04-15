bc10578319dc22b4ff6adcf190d6516534de9abd
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-04-15 17:56:26 +0200

    second part of first task done

--- tasks/homework1.py
+++ tasks/homework1.py
@@ -9,16 +9,29 @@ driver.find_element(By.LINK_TEXT, "File Upload").click()
 # Check current URL after the command:
 print(driver.current_url)
 
-file_path = os.path.abspath("images/khinkali.jpg")
-print(file_path)
-driver.find_element(By.ID, "file-upload").send_keys(file_path)
-driver.find_element(By.ID,"file-submit").submit()
+def upload_file(file_name):
+    try:
+        file_path = os.path.abspath(file_name)
+        print(file_path)
+        driver.find_element(By.ID, "file-upload").send_keys(file_path)
+        driver.find_element(By.ID,"file-submit").submit()
+        try:
+            assert driver.page_source.find("File Uploaded!")
+            time.sleep(5)
+            print("file upload success")
+        except:
+            print("file upload not successful")
 
-try:
-    assert driver.page_source.find("File Uploaded!")
-    time.sleep(5)
-    print("file upload success")
-except:
-    print("file upload not successful")
+    except:
+        print("File does not exist in the path")
 
-driver.quit()
+
+
+
+# Successful upload testing
+upload_file("images/khinkali.jpg")
+
+# Unsuccessful upload testing
+driver.back()
+print(driver.current_url)
+upload_file("hello.jpg")
