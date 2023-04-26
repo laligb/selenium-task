bdc685b3fd4a54156558c09dec1acb38975598ba
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-04-26 19:03:51 +0200

    allure installed

--- main.py
+++ main.py
@@ -2,7 +2,5 @@
 from tests.task1 import test_dropdown
 
 
-
-
 if __name__ == "main":
     test_dropdown
--- tests/task1/test_uploading.py
+++ tests/task1/test_uploading.py
@@ -1,37 +1,37 @@
-from tests.driver import *
-import time
-import os
+# from tests.driver import *
+# import time
+# import os
 
-# Task # 1
-print("Task #1: checking file uploading")
+# # Task # 1
+# print("Task #1: checking file uploading")
 
-# Check current URL:
-print(driver.current_url)
-driver.find_element(By.LINK_TEXT, "File Upload").click()
+# # Check current URL:
+# print(driver.current_url)
+# driver.find_element(By.LINK_TEXT, "File Upload").click()
 
-# Check current URL after the command:
-print(driver.current_url)
+# # Check current URL after the command:
+# print(driver.current_url)
 
-def upload_file(file_name):
-    try:
-        file_path = os.path.abspath(file_name)
-        print(file_path)
-        driver.find_element(By.ID, "file-upload").send_keys(file_path)
-        driver.find_element(By.ID,"file-submit").submit()
-        try:
-            assert driver.page_source.find("File Uploaded!")
-            print("file upload success")
-        except:
-            print("file upload not successful")
+# def upload_file(file_name):
+#     try:
+#         file_path = os.path.abspath(file_name)
+#         print(file_path)
+#         driver.find_element(By.ID, "file-upload").send_keys(file_path)
+#         driver.find_element(By.ID,"file-submit").submit()
+#         try:
+#             assert driver.page_source.find("File Uploaded!")
+#             print("file upload success")
+#         except:
+#             print("file upload not successful")
 
-    except:
-        print("File does not exist in the path")
+#     except:
+#         print("File does not exist in the path")
 
 
-# Successful upload testing
-upload_file("images/khinkali.jpg")
+# # Successful upload testing
+# upload_file("images/khinkali.jpg")
 
-# Unsuccessful upload testing
-driver.back()
-print(driver.current_url)
-upload_file("hello.jpg")
+# # Unsuccessful upload testing
+# driver.back()
+# print(driver.current_url)
+# upload_file("hello.jpg")
